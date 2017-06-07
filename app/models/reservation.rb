8570fbc20e85c9c387a32e4ecddc4ec9b04fd926
Owlaukka <olli.laukkanen@protonmail.com>
2017-06-08 00:23:56 +0300

    fix merge conflicts

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,10 +6,6 @@ class Reservation < ApplicationRecord
   def start_date_before_end_date
     if self.start > self.finish
       errors.add("Lopetus ennen aloitusta")
-<<<<<<< HEAD
-
-=======
->>>>>>> afdf936923cb4b55b110694f46287f5f0e81d94f
     end
   end
 end
