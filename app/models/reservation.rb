408f80ae12395fa32c0f420cbccb018c95016b76
NorthernLion <joppetaja@gmail.com>
2017-06-19 15:07:36 +0300

    Travis file kopioitu

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,19 @@ class Reservation < ApplicationRecord
   belongs_to :plane
 
   validate :start_date_before_end_date
+  validate :cannot_overlap_another_event
 
+  def cannot_overlap_another_event
+    range = Range.new start_at, end_at
+    overlaps = Event.exclude_self(id).in_range(range)
+    overlap_error unless overlaps.empty?
+  end
+
+  scope :in_range, -> range {
+    where('(start_at BETWEEN ? AND ?)', range.first, range.last)
+  }
+  scope :exclude_self, -> id { where.not(id: id) }
+  
   def start_date_before_end_date
     if self.start > self.finish
       errors.add("Lopetus ennen aloitusta")
