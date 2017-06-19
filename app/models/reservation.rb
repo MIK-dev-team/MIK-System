e84bf1e4834a300d1455cadcf1dca01dac651078
NorthernLion <joppetaja@gmail.com>
2017-06-19 15:18:19 +0300

    Validointi

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,17 +4,6 @@ class Reservation < ApplicationRecord
   validate :start_date_before_end_date
   validate :cannot_overlap_another_event
 
-  def cannot_overlap_another_event
-    range = Range.new start_at, end_at
-    overlaps = Event.exclude_self(id).in_range(range)
-    overlap_error unless overlaps.empty?
-  end
-
-  scope :in_range, -> range {
-    where('(start_at BETWEEN ? AND ?)', range.first, range.last)
-  }
-  scope :exclude_self, -> id { where.not(id: id) }
-  
   def start_date_before_end_date
     if self.start > self.finish
       errors.add("Lopetus ennen aloitusta")
