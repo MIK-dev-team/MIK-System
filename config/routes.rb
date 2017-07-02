6d148ab92515820a8ddf2e5b9a4460dc769c4513
sofiak <sofia.kansikas@gmail.com>
2017-07-02 21:10:16 +0300

    backend for auth #91, view for login #90

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   resources :reservations
   resources :planes, only: [:show]
   get '/planes/:id/reservations.json', to: 'planes#reservations'
   get 'hello/index'
   root 'hello#index'
+  get '/login', to: 'auth#show'
+  post '/login', to: 'auth#login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
