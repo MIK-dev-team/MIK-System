1f12f81a57b398d6045e93e147478c8cbaa8f60b
Owlaukka <olli.laukkanen@protonmail.com>
2017-08-10 05:16:42 +0300

    lokin luonnin kevyt testaus

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -47,5 +47,6 @@ class Reservation < ApplicationRecord
   private
     def publish_reservation_destroyed
       broadcast(:reservation_destroyed, self)
+      broadcast(:reservation_destroyed_for_log, self, false)
     end
 end
--- app/services/reservation_deletion_log_listener.rb
+++ app/services/reservation_deletion_log_listener.rb
@@ -1,5 +1,7 @@
 class ReservationDeletionLogListener
-  def self.reservation_destroyed (reservation)
+  def self.reservation_destroyed_for_log (res, by_admin)
+    log_entry = ReservationDeletionLog.new start:res.start, end:res.end, user_id:res.user_id, plane_id:res.plane_id, deleted_by_admin:by_admin
 
+    log_entry.save
   end
 end
